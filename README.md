e2030d7df1f3460a69a99eeefb0447e723c23ab2
Danilo Pereira <danilo4web@gmail.com>
2020-04-21 14:01:24 -0300

    README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+### How to unfollow everyone on linkedin:
+
+1. Go to here https://www.linkedin.com/feed/following/?filterType=connection 
+2. Open the chrome dev tools console 
+3. Paste the code from the script.js
+4. Done!
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Go to here https://www.linkedin.com/feed/following/?filterType=connection 
-// open the chrome dev tools console 
-// paste the following 
-// go grab a cup of tea
-// get rid of all life coach from linkedin 
-
 var buttons = $('button'), interval = setInterval(function() {
    var btn = $('.is-following');
    console.log('Clicking:', btn);
